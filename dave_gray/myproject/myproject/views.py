7a076801fcce49c0686ce1fe50b459d7a288ba72
Jansen Simanullang <jansen.simanullang@gmail.com>
2024-05-22 17:10:22 +0700

    spacing changes by formatter

--- dave_gray/myproject/myproject/views.py
+++ dave_gray/myproject/myproject/views.py
@@ -1,9 +1,11 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def homepage(request):
     # return HttpResponse("Helo World! I'm Home")
     return render(request, 'home.html')
+
 
 def about(request):
     # return HttpResponse("My About page.")
--- dave_gray/myproject/users/views.py
+++ dave_gray/myproject/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 
+
 # Create your views here.
 def register_view(request):
     if request.method == "POST":
@@ -12,6 +13,7 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 def login_view(request):
     if request.method == "POST":
@@ -25,6 +27,7 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
+
 
 def logout_view(request):
     if request.method == "POST":
